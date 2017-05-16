a68c02b0b12a32f2d4e79adea1558be99f39e9a3
CryptoCoinUser <adkantor@gmail.com>
2017-05-16 16:12:48 -0700

    displays coins in table

--- app/coinrouter.js
+++ app/coinrouter.js
@@ -3,15 +3,32 @@ var User  = require('../app/models/user');
 module.exports = function(app, passport) {
 
     app.post('/coin/add', isLoggedIn, (req, res) =>  {
-        console.log(req.user);
-        console.log(req.body);
-        User.findByIdAndUpdate(req.user._id, {
-            $push: {"coins": {"abrv": req.body.abrv, "qty": req.body.qty}}
-        },
-        {upsert: true, new : true},
-        function(err, user){
-            console.log(user);
-        });
+         console.log(req.user);
+         console.log(req.body);
+
+        let newCoin = req.body.abrv;
+        //if user alredy has this coin, update qty - later this can be done from table td
+        
+        // if(User.find({'abrv': newCoin})){
+        //     console.log(`already have ${newCoin}`);
+        // } else{
+
+           //if user doesn't have this coin yet
+            User.findByIdAndUpdate(req.user._id, {
+                $push: {"coins": {"abrv": req.body.abrv, "qty": req.body.qty}}
+
+            },
+            {upsert: true, new : true},
+            function(err, user){
+                console.log(user);
+
+            });
+
+       // } // else
+
+
+
+
     });
 
 
@@ -26,12 +43,35 @@ function isLoggedIn(req, res, next) {
  
 
  /*
-OCase.findByIdAndUpdate(req.params.id, {
-        $set: {
-            subject: req.body.subject,
-            description: req.body.description,
-            currentStep: req.body.currentStep
-        }
-    }, callback);
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+    },
+    function(req, email, password, done) {
+        if (email)
+            email = email.toLowerCase(); // Use lower-case e-mails to avoid case-sensitive e-mail matching
 
+        // asynchronous
+        process.nextTick(function() {
+            User.findOne({ 'local.email' :  email }, function(err, user) {
+                // if there are any errors, return the error
+                if (err)
+                    return done(err);
+
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+                // all is well, return user
+                else
+                    return done(null, user);
+            });
+        });
+
+    }));
  */
