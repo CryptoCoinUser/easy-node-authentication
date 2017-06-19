17baeca63eb8f48959ba1355cb470e1c937b6948
CryptoCoinUser <adkantor@gmail.com>
2017-06-19 14:21:55 -0700

    removed useless console.log

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
-console.log(process.env.AVRAM);
 // required for passport
 app.use(session({
     secret: 'ilovescotchscotchyscotchscotch', // session secret
